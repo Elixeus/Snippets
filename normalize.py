0f42c2691754770d6c0e1e659e4a8b1d7994e59b
Elixeus <eliseowang@gmail.com>
2016-04-10 18:59:53 -0400

    two different ways of normalization

--- normalize.py
+++ normalize.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn import preprocessing
 
 
+# method 1: bring the data between 0 and 1
 df = pd.DataFrame((range(5), range(6, 11)), columns=['a', 'b'])
 # Create a minimum and maximum processor object
 min_max_scaler = preprocessing.MinMaxScaler()
@@ -10,3 +11,6 @@ df_scaled = min_max_scaler.fit_transform(df)
 # Run the normalizer on the dataframe
 df_norm = pd.DataFrame(df_scaled, columns=df.columns)
 df_norm.head(5)
+
+# method2: bring the data to a unit distribution
+df_stand = preprocessing.scale(df)
