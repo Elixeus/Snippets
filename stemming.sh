94dea748ed0f77db61d28a69f72433602ba948ba
Elixeus <eliseowang@gmail.com>
2016-04-17 22:52:12 -0400

    correct the sys.argv

--- stemming.sh
+++ stemming.sh
@@ -4,5 +4,5 @@ if [ $# -eq 0 ]
 then
     echo 'No argument passed. Command format: stemming.sh foo.txt'
 else
-    tr -sc 'A-Za-z' '\n' < Shake.txt | tr 'A-Z' 'a-z' | grep '[aeiou].*ing$' | sort | uniq -c | sort -n -r | less
+    tr -sc 'A-Za-z' '\n' < $1 | tr 'A-Z' 'a-z' | grep '[aeiou].*ing$' | sort | uniq -c | sort -n -r | less
 fi
