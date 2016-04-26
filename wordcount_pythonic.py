32c176fec7ea04eb546f418baae94539ce9b2d1a
Elixeus <eliseowang@gmail.com>
2016-04-25 22:03:25 -0400

    in reversed order

--- wordcount_pythonic.py
+++ wordcount_pythonic.py
@@ -9,6 +9,8 @@ with open('file.txt', 'r') as fh:
         for word in words:
             counts[word] = counts.get(word, 0) + 1
 # sort the dict by value
-sorted_counts = sorted(counts.items(), key=operator.itemgetter[1])
+sorted_counts = sorted(counts.items(),
+                       key=operator.itemgetter[1],
+                       reverse=True)
 # sorted_counts is a list of key-value tuples sorted from high to low
 print sorted_counts
