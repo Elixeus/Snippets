e5cf47d9e02d8f7a62b914456dfa44c1ee756cbb
Elixeus <eliseowang@gmail.com>
2016-04-25 22:14:54 -0400

    take care of the punctuations, add a second way

--- wordcount_pythonic.py
+++ wordcount_pythonic.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import operator
+from string import punctuation
 
-
+# first way
 counts = {}
 with open('file.txt', 'r') as fh:
     for line in fh:
-        words = line.split()
+        words = line.lower().translate(None, punctuation).split()
         for word in words:
             counts[word] = counts.get(word, 0) + 1
 # sort the dict by value
@@ -13,4 +14,17 @@ sorted_counts = sorted(counts.items(),
                        key=operator.itemgetter[1],
                        reverse=True)
 # sorted_counts is a list of key-value tuples sorted from high to low
+print sorted_counts
+
+
+# second way
+counts = {}
+with open('file.txt', 'r') as fh:
+    text = fh.read()
+word_ls = text.lower().translate(None, punctuation).split()
+for word in word_ls:
+    counts[word] = word_ls.count(word)
+sorted_counts = sorted(counts.items(),
+                       key=operator.itemgetter[1],
+                       reverse=True)
 print sorted_counts
