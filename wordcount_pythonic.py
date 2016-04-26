5409394e0fb594330b47ae95dcd6d889cbd164d0
Elixeus <eliseowang@gmail.com>
2016-04-25 21:59:47 -0400

    word count in python using get and sort by value

---
+++ wordcount_pythonic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import operator
+
+
+counts = {}
+with open('file.txt', 'r') as fh:
+    for line in fh:
+        words = line.split()
+        for word in words:
+            counts[word] = counts.get(word, 0) + 1
+# sort the dict by value
+sorted_counts = sorted(counts.items(), key=operator.itemgetter[1])
+# sorted_counts is a list of key-value tuples sorted from high to low
+print sorted_counts
