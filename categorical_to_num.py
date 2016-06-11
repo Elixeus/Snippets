93c6abe0bfd51ecc74ecb44bd617a4b5fb23d0ea
Elixeus <xw876@nyu.edu>
2016-06-11 01:25:28 -0400

    change to a funciton

--- categorical_to_num.py
+++ categorical_to_num.py
@@ -9,7 +9,26 @@ called data['category'])
 '''
 
 
-ls = list(set(data['category']))
-#cat_dict = dict((j, i) for i, j in enumerate(ls))
-cat_dict = {j: i for i, j in enumerate(ls)}
-data['category'] = map(lambda x: cat_dict[x], data['category'])
+# ls = list(set(data['category']))
+# #cat_dict = dict((j, i) for i, j in enumerate(ls))
+# cat_dict = {j: i for i, j in enumerate(ls)}
+# data['category'] = map(lambda x: cat_dict[x], data['category'])
+
+
+def categorical_to_num(categos):
+    '''
+    convert the categorical variables to dummy numbers.
+    return a list that contains a dummy number corresponding to each
+    categorical value.
+
+    parameters:
+    ------------
+    categos: the iterable that contains all the categorical values'''
+    cat_dict = {cate: num for num, cate in
+                enumerate(sorted(list(set(categos))))}
+    return tuple(map(lambda x: cat_dict[x], categos))
+
+
+if __name__ == '__main__':
+    ls = list('ACBDE')
+    print categorical_to_num(ls)
